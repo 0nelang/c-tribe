f70fcc826f35d3164de90ecb9f1806dd9e28dead
Hilzam <hilzammm@gmail.com>
2022-01-12 16:13:09 +0700

    merapihkan view general

--- resources/views/dashboard/general.blade.php
+++ resources/views/dashboard/general.blade.php
@@ -2,84 +2,87 @@
 
 @section('main')
 
-    <div class="page-content">
-        <div class="main-wrapper">
+<div class="page-content">
+    <div class="main-wrapper">
         <div class="row">
             <div class="col">
-            <div class="card">
-                <div class="card-body">
-                <h5 class="card-title">Navbar</h5>
-                <p class="card-description">Here’s a quick example to demonstrate Bootstrap’s form styles. Keep reading
-                    for documentation on required classes, form layout, and more.</p>
-                <form>
-                    <div class="mb-3">
-                        <label for="formFile" class="form-label">Brand Image</label>
-                        <input class="form-control" type="file" id="formFile">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Navbar</h5>
+                        <p class="card-description">Here’s a quick example to demonstrate Bootstrap’s form styles. Keep
+                            reading
+                            for documentation on required classes, form layout, and more.</p>
+                        <form action="" method="" enctype="multipart/form-data">
+                            <div class="mb-3">
+                                <label for="formFile" class="form-label">Brand Image</label>
+                                <input class="form-control" type="file" id="formFile">
+                            </div>
+                            <div class="mb-3">
+                                <label for="" class="form-label">Description</label>
+                                <input type="email" class="form-control" id="" aria-describedby="emailHelp">
+                            </div>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </form>
                     </div>
-                    <div class="mb-3">
-                    <label for="" class="form-label">Description</label>
-                    <input type="email" class="form-control" id="" aria-describedby="emailHelp">
-                    </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </form>
                 </div>
-            </div>
-        <div class="row">
-            <div class="col">
-            <div class="card">
-                <div class="card-body">
-                <h5 class="card-title">Footer</h5>
-                <p class="card-description">Here’s a quick example to demonstrate Bootstrap’s form styles. Keep reading
-                    for documentation on required classes, form layout, and more.</p>
-                <form>
-                    <div class="mb-3">
-                        <label for="formFile" class="form-label">Background Image</label>
-                        <input class="form-control" type="file" id="formFile">
-                    </div>
-                    <div class="mb-3">
-                        <label for="formFile" class="form-label">Brand Image</label>
-                        <input class="form-control" type="file" id="formFile">
-                    </div>
-                    <div class="mb-3">
-                        <label for="" class="form-label">Email</label>
-                        <input type="email" class="form-control" id="" aria-describedby="emailHelp">
-                    </div>
-                    <div class="mb-3">
-                        <label for="" class="form-label">Phone</label>
-                        <input type="email" class="form-control" id="" aria-describedby="emailHelp">
-                    </div>
-                    <div class="mb-3">
-                        <label for="" class="form-label">Social</label>
-                        <input type="email" class="form-control" id="" aria-describedby="emailHelp">
-                    </div>
-                    <div class="mb-3">
-                        <label for="" class="form-label">Address</label>
-                        <input type="email" class="form-control" id="" aria-describedby="emailHelp">
-                    </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </form>
-                </div>
-            </div>
-        <div class="row">
-            <div class="col">
-            <div class="card">
-                <div class="card-body">
-                <h5 class="card-title">General</h5>
-                <p class="card-description">Here’s a quick example to demonstrate Bootstrap’s form styles. Keep reading
-                    for documentation on required classes, form layout, and more.</p>
-                <form>
-                    <div class="mb-3">
-                        <label for="formFile" class="form-label">Cursor Image</label>
-                        <input class="form-control" type="file" id="formFile">
-                    </div>
-                    <div class="mb-3">
-                        <label for="formFile" class="form-label">Hover Image Menu</label>
-                        <input class="form-control" type="file" id="formFile">
-                    </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </form>
-                </div>
-            </div>
-            </div>
-        </div>
-@endsection
+                <div class="row">
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">Footer</h5>
+                                <p class="card-description">Here’s a quick example to demonstrate Bootstrap’s form
+                                    styles. Keep reading
+                                    for documentation on required classes, form layout, and more.</p>
+                                <form action="" method="" enctype="multipart/form-data">
+                                    <div class="mb-3">
+                                        <label for="formFile" class="form-label">Background Image</label>
+                                        <input class="form-control" type="file" id="formFile">
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="formFile" class="form-label">Brand Image</label>
+                                        <input class="form-control" type="file" id="formFile">
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="" class="form-label">Email</label>
+                                        <input type="email" class="form-control" id="" aria-describedby="emailHelp">
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="" class="form-label">Phone</label>
+                                        <input type="email" class="form-control" id="" aria-describedby="emailHelp">
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="" class="form-label">Social</label>
+                                        <input type="email" class="form-control" id="" aria-describedby="emailHelp">
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="" class="form-label">Address</label>
+                                        <input type="email" class="form-control" id="" aria-describedby="emailHelp">
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </form>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h5 class="card-title">General</h5>
+                                        <p class="card-description">Here’s a quick example to demonstrate Bootstrap’s
+                                            form styles. Keep reading
+                                            for documentation on required classes, form layout, and more.</p>
+                                        <form action="" method="" enctype="multipart/form-data">
+                                            <div class="mb-3">
+                                                <label for="formFile" class="form-label">Cursor Image</label>
+                                                <input class="form-control" type="file" id="formFile">
+                                            </div>
+                                            <div class="mb-3">
+                                                <label for="formFile" class="form-label">Hover Image Menu</label>
+                                                <input class="form-control" type="file" id="formFile">
+                                            </div>
+                                            <button type="submit" class="btn btn-primary">Submit</button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        @endsection
--- resources/views/layouts/dashboard-main.blade.php
+++ resources/views/layouts/dashboard-main.blade.php
@@ -33,11 +33,11 @@
     <![endif]-->
 </head>
 <body>
-    <div class='loader'>
+    {{-- <div class='loader'>
     <div class='spinner-grow text-primary' role='status'>
         <span class='sr-only'>Loading...</span>
     </div>
-    </div>
+    </div> --}}
     <div class="page-container">     
         @include('partials.dashboard.page-header')
     
