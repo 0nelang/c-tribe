0654fc6e254645059f4b9af3e4d2f3f6045c00e9
Hilzam <hilzammm@gmail.com>
2022-01-12 10:32:20 +0700

    menambahkan table general

---
+++ database/migrations/2022_01_12_032742_create_generals_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGeneralsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('generals', function (Blueprint $table) {
+            $table->id();
+            $table->string('brand_navbar');
+            $table->string('description_navbar');
+            $table->string('cursor_img');
+            $table->string('brand_footer');
+            $table->string('email_footer');
+            $table->string('phone_footer');
+            $table->string('sosial_footer');
+            $table->string('addres_footer');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('generals');
+    }
+}
