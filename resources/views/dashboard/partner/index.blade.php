5e7a0c3f297a487b753f646d0a5df18d9a98e0b8
Hilzam <hilzammm@gmail.com>
2022-01-13 12:11:24 +0700

    menambah table di index partner

--- resources/views/dashboard/partner/index.blade.php
+++ resources/views/dashboard/partner/index.blade.php
@@ -16,42 +16,26 @@
                             <thead>
                                 <tr>
                                     <th scope="col">#</th>
-                                    <th scope="col">image</th>
-                                    <th scope="col">name</th>
+                                    <th scope="col">logo</th>
+                                    <th scope="col">index</th>
                                     <th scope="col">title</th>
-                                    <th scope="col">desc</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 <tr>
                                     <th scope="row">1</th>
-                                    <td>Mark</td>
+                                    <td><img src=".." alt=".."></td>
                                     <td>Otto</td>
-                                    <td>@mdo</td>
-                                    <td>@mdo</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">2</th>
-                                    <td>Jacob</td>
-                                    <td>Thornton</td>
-                                    <td>@fat</td>
-                                    <td>@mdo</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">3</th>
-                                    <td colspan="2">Larry the Bird</td>
-                                    <td>@twitter</td>
                                     <td>@mdo</td>
                                 </tr>
                             </tbody>
-
                         </table>
                     </div>
                 </div>
             </div>
         </div>
 
-</div>
+    </div>
 
 
-@endsection
+    @endsection
