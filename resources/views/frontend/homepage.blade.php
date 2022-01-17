3ade889bdf3c52f56042d16600ae25984460df02
Hilzam <hilzammm@gmail.com>
2022-01-17 15:53:44 +0700

    membuat head terpecah menjadi partial

--- resources/views/frontend/homepage.blade.php
+++ resources/views/frontend/homepage.blade.php
@@ -14,32 +14,7 @@
   <body>
     <div class="cursor"></div>
     <!-- HEADER-->
-    <header class="header header__black" id="header">
-      <div class="header__wrap">
-        <div class="header__logo"><a href="homepage.html"><img class="logo-1" src="/images/logo-ctribe.svg"/><img class="logo-2" src="/images/logo-ctribe-white.svg"/></a></div>
-        <div class="header__text"><span>WE ARE NOT YOUR AGENCY WE ARE SOMETHING BETTER</span></div>
-        <div class="header__menu">
-          <div class="menu-toggle"><span>MENU</span><span class="close">CLOSE</span></div>
-        </div>
-      </div>
-      <div class="main-menu">
-        <div class="main-menu__wrap">
-          <div class="main-menu__fade-top"></div>
-          <div class="main-menu__fade-bot"></div>
-          <div class="main-menu__bg">
-            <div class="bg-hover"></div><span></span>
-          </div>
-          <ul>
-            <li class="menu-item"><a class="menu-link" href="project.html" data-bg="/images/img-bg-menu-inpsirtaion.jpg" data-text="PROJECT">PROJECT</a></li>
-            <li class="menu-item"><a class="menu-link" href="our-people.html" data-bg="/images/img-bg-menu-inpsirtaion.jpg" data-text="PEOPLE">PEOPLE</a></li>
-            <li class="menu-item"><a class="menu-link" href="tribes.html" data-bg="/images/img-bg-menu-inpsirtaion.jpg" data-text="TRIBES">TRIBES</a></li>
-            <li class="menu-item"><a class="menu-link" href="flagship.html" data-bg="/images/img-bg-menu-inpsirtaion.jpg" data-text="FLAGSHIP">FLAGSHIP</a></li>
-            <li class="menu-item"><a class="menu-link" href="#" data-bg="/images/img-bg-menu-inpsirtaion.jpg" data-text="SHOP">SHOP</a></li>
-            <li class="menu-item"><a class="menu-link" href="inspiration.html" data-bg="/images/img-bg-menu-inpsirtaion.jpg" data-text="INSPIRATION">INSPIRATION</a></li>
-          </ul>
-        </div>
-      </div>
-    </header>
+    @include('frontend.partial.head')
     <main class="page-home">
       <section class="page-home__one cursor-img header-2">
         <div class="wrap">
---
+++ resources/views/frontend/partial/head.blade.php
@@ -0,0 +1,26 @@
+<header class="header header__black" id="header">
+    <div class="header__wrap">
+      <div class="header__logo"><a href="homepage.html"><img class="logo-1" src="/images/logo-ctribe.svg"/><img class="logo-2" src="/images/logo-ctribe-white.svg"/></a></div>
+      <div class="header__text"><span>WE ARE NOT YOUR AGENCY WE ARE SOMETHING BETTER</span></div>
+      <div class="header__menu">
+        <div class="menu-toggle"><span>MENU</span><span class="close">CLOSE</span></div>
+      </div>
+    </div>
+    <div class="main-menu">
+      <div class="main-menu__wrap">
+        <div class="main-menu__fade-top"></div>
+        <div class="main-menu__fade-bot"></div>
+        <div class="main-menu__bg">
+          <div class="bg-hover"></div><span></span>
+        </div>
+        <ul>
+          <li class="menu-item"><a class="menu-link" href="/frontend/project" data-bg="/images/img-bg-menu-inpsirtaion.jpg" data-text="PROJECT">PROJECT</a></li>
+          <li class="menu-item"><a class="menu-link" href="/frontend/our-people" data-bg="/images/img-bg-menu-inpsirtaion.jpg" data-text="PEOPLE">PEOPLE</a></li>
+          <li class="menu-item"><a class="menu-link" href="/frontend/tribes" data-bg="/images/img-bg-menu-inpsirtaion.jpg" data-text="TRIBES">TRIBES</a></li>
+          <li class="menu-item"><a class="menu-link" href="/frontend/flagship" data-bg="/images/img-bg-menu-inpsirtaion.jpg" data-text="FLAGSHIP">FLAGSHIP</a></li>
+          <li class="menu-item"><a class="menu-link" href="#" data-bg="/images/img-bg-menu-inpsirtaion.jpg" data-text="SHOP">SHOP</a></li>
+          <li class="menu-item"><a class="menu-link" href="/frontend/inspiration" data-bg="/images/img-bg-menu-inpsirtaion.jpg" data-text="INSPIRATION">INSPIRATION</a></li>
+        </ul>
+      </div>
+    </div>
+  </header>
