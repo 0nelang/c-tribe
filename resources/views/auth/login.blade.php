d4abb22936a59b485907c8eb49462256c8b80a7a
0nelang <elangppranoto@gmail.com>
2022-01-19 17:15:05 +0700

    membuat login admin

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Responsive Admin Dashboard Template">
+        <meta name="keywords" content="admin,dashboard">
+        <meta name="author" content="stacks">
+        <!-- The above 6 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+        <!-- Title -->
+        <title>Circl - Responsive Admin Dashboard Template</title>
+
+        <!-- Styles -->
+        <link href="https://fonts.googleapis.com/css?family=Poppins:400,500,700,800&display=swap" rel="stylesheet">
+        <link href="/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link href="/plugins/font-awesome/css/all.min.css" rel="stylesheet">
+        <link href="/plugins/perfectscroll/perfect-scrollbar.css" rel="stylesheet">
+
+
+        <!-- Theme Styles -->
+        <link href="/css/main.min.css" rel="stylesheet">
+        <link href="/css/dark-theme.css" rel="stylesheet">
+        <link href="/css/custom.css" rel="stylesheet">
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body class="login-page">
+        <div class='loader'>
+            <div class='spinner-grow text-primary' role='status'>
+              <span class='sr-only'>Loading...</span>
+            </div>
+          </div>
+        <div class="container">
+            <div class="row justify-content-md-center">
+                <div class="col-md-12 col-lg-4">
+                    <div class="card login-box-container">
+                        <div class="card-body">
+                            <div class="authent-text">
+                                <p>Please Sign-in to your account.</p>
+                            </div>
+
+                            <form method="POST" action="{{ route('login') }}">
+                                @csrf
+                                <div class="mb-3">
+                                    <div class="form-floating">
+                                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                        <label for="emai">Email address</label>
+                                        @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                    </div>
+                                </div>
+                                <div class="mb-3">
+                                    <div class="form-floating">
+                                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                        <label for="floatingPassword">Password</label>
+                                        @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                                      </div>
+                                </div>
+                                <div class="d-grid">
+                                <button type="submit" class="btn btn-info m-b-xs">Sign In</button>
+                                </div>
+                              </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <!-- Javascripts -->
+        <script src="/plugins/jquery/jquery-3.4.1.min.js"></script>
+        <script src="https://unpkg.com/@popperjs/core@2"></script>
+        <script src="/plugins/bootstrap/js/bootstrap.min.js"></script>
+        <script src="https://unpkg.com/feather-icons"></script>
+        <script src="/plugins/perfectscroll/perfect-scrollbar.min.js"></script>
+        <script src="/js/main.min.js"></script>
+    </body>
+</html>
+
---
+++ resources/views/dashboard/admin.blade.php
@@ -0,0 +1,71 @@
+@extends('layouts.dashboard-main')
+
+@section('main')
+
+    <div class="page-content">
+        <div class="main-wrapper">
+
+            <div class="row">
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <form action="/admin/reset" method="POST"id="admin-pass">
+                                @method("Put")
+                                @csrf
+                                <input type="hidden" value="{{ auth()->user()->id }}" name="id">
+                                <div class="mb-3">
+                                    <label for="email" class="form-label">Email</label>
+                                    <div class="mb-3">
+                                    <img id="output" class="mb-3">
+                                    </div>
+                                    <input class="form-control @error('email') is-invalid @enderror" name="email" type="text" id="email" value="{{ auth()->user()->email }}">
+                                    @error('email')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                  @enderror
+                                </div>
+                                <div class="mb-3">
+                                    <label for="admin" class="form-label">New Password</label>
+                                    <div class="mb-3">
+                                    <img id="output" class="mb-3">
+                                    </div>
+                                    <input class="form-control @error('password') is-invalid @enderror" name="password" type="password" id="admin">
+                                    @error('password')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                  @enderror
+                                </div>
+                                <button class="btn btn-primary" onclick="change()">Submit</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+    </div>
+    <script>
+        function what(id) {
+            Swal.fire({
+                title: 'Are you sure?',
+                text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    $('#admin-pass').submit();
+                    Swal.fire(
+                        'Deleted!',
+                        'Your file has been deleted.',
+                        'success'
+                    )
+                }
+            })
+        }
+
+    </script>
+@endsection
