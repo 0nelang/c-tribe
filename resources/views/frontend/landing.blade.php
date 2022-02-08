db8dd59ebdd0a098261ad4536dba7c1b953ab6e1
0nelang <88448211+0nelang@users.noreply.github.com>
2022-02-08 11:52:33 +0700

    text home

--- resources/views/frontend/landing.blade.php
+++ resources/views/frontend/landing.blade.php
@@ -11,7 +11,7 @@
             <video src="{{ asset('storage/' . $general->video_background) }}" type="video/mp4" playsinline="playsinline"
                 autoplay="autoplay" loop="loop" muted="muted" id="video-bg"></video>
         </div>
-        <div class="wrap">
+        <div class="wrap teks-home">
             <h1 class="page-landing__title">{!! $landing1->title !!}</h1><span class="page-landing__date"><img
                     class="ic" src="/images/icon-01.svg" alt="Icon" /><img class="ic"
                     src="/images/icon-01.svg" alt="Icon" /><span class="date">{!! $landing1->sub_title !!}</span><img
