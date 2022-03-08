21613ef87deed42bf609c32618bbef62bf579493
0nelang <88448211+0nelang@users.noreply.github.com>
2022-03-08 13:51:27 +0700

    membuat table manu

---
+++ database/migrations/2022_03_08_064354_create_menus_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->id();
+            $table->string('default_name');
+            $table->string('default_url');
+            $table->string('custom_name')->nullable();
+            $table->string('custom_url')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menus');
+    }
+}
