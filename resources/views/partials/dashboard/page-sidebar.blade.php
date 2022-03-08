eec72b7e81d97664bdef14b6d9e574d579e275f7
HIilLm <97495105+HIilLm@users.noreply.github.com>
2022-03-08 16:45:51 +0700

    ha

--- resources/views/partials/dashboard/page-sidebar.blade.php
+++ resources/views/partials/dashboard/page-sidebar.blade.php
@@ -6,7 +6,7 @@
           <li class="{{ ($page === 'Admin')? 'active-page' : ''  }}">
             <a href="/admin"><i data-feather="home"></i>Dashboard</a>
           </li>
-          
+
           <li class="sidebar-title">
             Apps
           </li>
@@ -31,9 +31,7 @@
             <a href="/admin/project"><i data-feather="package"></i>Project</a>
           </li>
           <li class="{{ ($page === 'Inspiration')? 'active-page' : ''  }}">
-            <a href="/admin/inspiration"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lightbulb" viewBox="0 0 16 16">
-              <path d="M2 6a6 6 0 1 1 10.174 4.31c-.203.196-.359.4-.453.619l-.762 1.769A.5.5 0 0 1 10.5 13a.5.5 0 0 1 0 1 .5.5 0 0 1 0 1l-.224.447a1 1 0 0 1-.894.553H6.618a1 1 0 0 1-.894-.553L5.5 15a.5.5 0 0 1 0-1 .5.5 0 0 1 0-1 .5.5 0 0 1-.46-.302l-.761-1.77a1.964 1.964 0 0 0-.453-.618A5.984 5.984 0 0 1 2 6zm6-5a5 5 0 0 0-3.479 8.592c.263.254.514.564.676.941L5.83 12h4.342l.632-1.467c.162-.377.413-.687.676-.941A5 5 0 0 0 8 1z"/>
-            </svg>Insipiration</a>
+            <a href="/admin/inspiration"><i data-feather="sun"></i>Insipiration</a>
           </li>
           <li class="{{ ($page === 'Flagship')? 'active-page' : ''  }}">
             <a href="/admin/flagship"><i data-feather="flag"></i>Flagship</a>
