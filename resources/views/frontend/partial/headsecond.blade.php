5ecc4c9c4a75a43d3f86476ba95ae95b5431a33a
0nelang <88448211+0nelang@users.noreply.github.com>
2022-03-09 15:48:47 +0700

    custom url front end

--- resources/views/frontend/partial/headsecond.blade.php
+++ resources/views/frontend/partial/headsecond.blade.php
@@ -22,7 +22,18 @@
             @foreach ($menu as $m)
             <li class="menu-item @if ($m->disabled == true) disabled @endif"><a class="menu-link"
                 @if ($m->custom == true)
-                href="{{ route('lol', ['menu' => $m->custom_url]) }}" data-bg="{{ asset('storage/' . $general->hover_image) }}" data-text="{{ $m->custom_name }}">{{ $m->custom_name }}</a></li>
+                    @if ($m->custom_url != null)
+                        href="{{ route('lol', ['menu' => $m->custom_url]) }}"
+                    @else
+                    href="{{ route('lol', ['menu' => $m->default_url]) }}"
+                    @endif
+
+                    @if ($m->custom_name != null)
+                    data-bg="{{ asset('storage/' . $general->hover_image) }}" data-text="{{ $m->custom_name }}">{{ $m->custom_name }}</a></li>
+                    @else
+                    data-bg="{{ asset('storage/' . $general->hover_image) }}" data-text="{{ $m->default_name }}">{{ $m->default_name }}</a></li>
+
+                    @endif
                 @else
                 href="{{ $m->default_url }}" data-bg="{{ asset('storage/' . $general->hover_image) }}" data-text="{{ $m->default_name }}">{{ $m->default_name }}</a></li>
                 @endif
